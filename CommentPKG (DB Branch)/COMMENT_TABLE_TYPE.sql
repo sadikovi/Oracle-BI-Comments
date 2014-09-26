d0a7235ed03d88212ea43a91baa14133190ff506
sadikovi <sadikovi@users.noreply.github.com>
2014-09-26 21:49:45 +1200

    Rename COMMENT_TABLE_TYPE.tps to COMMENT_TABLE_TYPE.sql

---
+++ CommentPKG (DB Branch)/COMMENT_TABLE_TYPE.sql
@@ -0,0 +1,3 @@
+--òèï, îïðåäåëÿþùèé òàáëèöó âîçâðàùàåìûõ äàííûõ äëÿ òèïà COMMENT_RETURN_TYPE
+create or replace type comment_table_type as table of comment_return_type
+/
--- CommentPKG (DB Branch)/COMMENT_TABLE_TYPE.tps
+++
@@ -1,3 +0,0 @@
---тип, определяющий таблицу возвращаемых данных для типа COMMENT_RETURN_TYPE
-create or replace type comment_table_type as table of comment_return_type
-/
